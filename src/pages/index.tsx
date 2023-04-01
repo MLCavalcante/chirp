f472afba937485c82e5a00c05ea794a0429091e6
MLCavalcante <mariacova7@outlook.com>
2023-04-01 12:39:25 -0300

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,11 @@ import { SignInButton, useUser, SignIn , SignOutButton} from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  
 
   const user = useUser()
+
+  const {data} = api.posts.getAll.useQuery()
 
   return ( 
     <>
@@ -23,7 +25,8 @@ const Home: NextPage = () => {
         <div>
         {!user.isSignedIn && <SignInButton />}{!!user.isSignedIn && <SignOutButton />}
         </div>
-          <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>{data?.map((post) => (<div key={post.id}>{post.content}</div>))}</div>
+         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
       </main>
     </>
   );
