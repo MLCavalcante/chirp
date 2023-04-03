f9827347344dd70752feb4b7b2cf88ee4815ebfc
MLCavalcante <mariacova7@outlook.com>
2023-04-03 14:51:05 -0300

    working posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,12 +13,24 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage } from "~/components/loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () =>{
 
   const {user} = useUser()
+
+  const [input, setInput] = useState("");
+
+  const ctx = api.useContext()
+
+  const {mutate, isLoading: isPosting} =  api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.posts.getAll.invalidate();
+    }
+  })
 
   console.log(user)
 
@@ -34,7 +46,12 @@ const CreatePostWizard = () =>{
       /> 
       <input placeholder="Type some emojis!" 
       className="grow bg-transparent outline-none"
+      type="text"
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+      disabled={isPosting}
       />
+      <button onClick={() => mutate({content: input})}>Post</button>
     </div>
   ); 
 }
@@ -77,7 +94,7 @@ const Feed = () => {
   if (!data) return <div>Something went wrong...</div>;
 
   return (
-      <div className="flex flex-col">{[...data,...data]?.map((fullPost) => (
+      <div className="flex flex-col">{data.map((fullPost) => (
       <PostView {...fullPost} key={fullPost.post.id}/>
       ))}
       </div>
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -2,6 +2,7 @@ import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import { privateProcedure } from "~/server/api/trpc";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
@@ -16,7 +17,10 @@ const filterUserForCliente = (user: User) => {
 export const postsRouter = createTRPCRouter({
 getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
-      take: 100, 
+      take: 100,
+      orderBy: [
+        {createdAt: 'desc'}
+      ] 
     });
 
     const users = (
@@ -43,7 +47,27 @@ getAll: publicProcedure.query(async ({ ctx }) => {
         username: author.username,
       },
     }
-  });
+  
+   });
 
   }),
+
+  create: privateProcedure
+  .input(z.object({
+    content: z.string().emoji().min(1).max(280),
+  })
+  )
+  .mutation(async ({ctx, input}) => {
+  const authorId = ctx.userId;
+
+  const post = await ctx.prisma.post.create({
+    data: {
+      authorId,
+      content:input.content,
+    },
+  })
+
+  return post;
+
+  })
 });
