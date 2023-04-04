84c5a14d385f967cc86f07338d491248144cad52
MLCavalcante <mariacova7@outlook.com>
2023-04-04 12:31:28 -0300

    new files for new routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -92,10 +92,10 @@ const PostView = (props: PostWithUSer) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <span>{`@${author.username} `}</span> 
-          <span className="font-thin">{` · ${dayjs(
+          <Link href={`/@${author.username}`}><span>{`@${author.username} `}</span></Link> 
+          <Link href={`/post/${post.id}`}><span className="font-thin">{` · ${dayjs(
             post.createdAt
-            ).fromNow()}`}</span>
+            ).fromNow()}`}</span></Link>
           </div>
       <span className="text-2xl">{post.content}</span>
       </div>
