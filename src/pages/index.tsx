f5a9d68da4f588ea0ae938bf894004a91da00aed
MLCavalcante <mariacova7@outlook.com>
2023-04-03 23:25:57 -0300

    Posting loading states, toast

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,12 +8,13 @@ import { SignInButton, useUser, SignIn , SignOutButton} from "@clerk/nextjs";
 
 import { api } from "~/utils/api";
 import type { RouterOutputs } from "~/utils/api";
-
+import { toast } from "react-hot-toast";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
+
 
 dayjs.extend(relativeTime);
 
@@ -29,6 +30,14 @@ const CreatePostWizard = () =>{
     onSuccess: () => {
       setInput("");
       void ctx.posts.getAll.invalidate();
+    },
+    onError:(e)=>{
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post! Please try again later.");
+      }
     }
   })
 
@@ -49,9 +58,20 @@ const CreatePostWizard = () =>{
       type="text"
       value={input}
       onChange={(e) => setInput(e.target.value)}
+      onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if(input !== ""){
+              mutate({content: input})
+            }
+        }
+      }}
       disabled={isPosting}
       />
+      {input !== "" && !isPosting && (
       <button onClick={() => mutate({content: input})}>Post</button>
+        )}
+      {isPosting && <div className="flex justify-center items-center"><LoadingSpinner size={20} /></div>}
     </div>
   ); 
 }
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -2,9 +2,11 @@ import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-import { privateProcedure } from "~/server/api/trpc";
-
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { } from "~/server/api/trpc";
+import {privateProcedure, 
+        createTRPCRouter,
+        publicProcedure } 
+   from "~/server/api/trpc";
 
 const filterUserForCliente = (user: User) => {
   return {  
@@ -65,13 +67,13 @@ getAll: publicProcedure.query(async ({ ctx }) => {
 
   create: privateProcedure
   .input(z.object({
-    content: z.string().emoji().min(1).max(280),
+    content: z.string().emoji("Only emojis are allowed").min(1).max(280),
   })
   )
   .mutation(async ({ctx, input}) => {
   const authorId = ctx.userId;
 
-  const {success} = await ratelimit.limit(authorId);
+  const {success} = await ratelimit.limit(authorId); 
   if (!success) throw new TRPCError({code: "TOO_MANY_REQUESTS"})
 
   const post = await ctx.prisma.post.create({
