59cd5682198dfc5a9a85e779da7291cb96f873cc
MLC <mariacova7@outlook.com>
2023-05-07 20:20:12 -0300

    Update

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,88 +1,84 @@
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
-
 import { PageLayout } from "~/components/layout";
 import Image from "next/image";
 import { LoadingPage } from "~/components/loading";
 import { PostView } from "~/components/postview";
-import {generateSSGHelper} from "~/server/helpers/ssgHelper"
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 
-const ProfileFeed = (props:{userId: string}) => {
+const ProfileFeed = (props: { userId: string }) => {
+  const { data, isLoading } = api.posts.getPostsByUserId.useQuery({
+    userId: props.userId,
+  });
 
-  const {data, isLoading} = api.posts.getPostsByUserId.useQuery({userId:props.userId})
+  if (isLoading) return <LoadingPage />;
 
-  if (isLoading) return <LoadingPage/>;
+  if (!data || data.length === 0) return <div>User has not posted</div>;
 
-  if (!data || data.length === 0 ) return <div>User has not posted</div>;
-
-  return (<div className="flex flex-col">
-   {data.map((fullPost) => (<PostView {...fullPost} key={fullPost.post.id}/>))}
-
-  </div>
-);
+  return (
+    <div className="flex flex-col">
+      {data.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}
+    </div>
+  );
 };
 
-
-
-
-
-const ProfilePage: NextPage<{username: string}> = ({username}) => {
-  
-
-  const {data} = api.profile.getUserByUsername.useQuery({
+const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
+  const { data } = api.profile.getUserByUsername.useQuery({
     username,
   });
-  
   if (!data) return <div>404</div>;
-
-  return ( 
+  return (
     <>
       <Head>
-        <title>{data.username}</title>
+        <title>{data.username ?? data.externalUsername}</title>
       </Head>
       <PageLayout>
-        <div className="relative h-36 border-b bg-slate-600">
-          <Image src={data.profileImageUrl} 
-          alt= {`${data.username ?? ""}'s profile pic`}
-          width={128}
-          height={128}
-          className="-mb-[64px] absolute bottom-0 left-0 rounded-full border-4 border-black bg-black ml-4"
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`${
+              data.username ?? data.externalUsername ?? "unknown"
+            }'s profile pic`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
           />
-          </div>
-          <div className= "h-[64px]"></div>
-          <div className="p-4 text-2xl font-bold">{`@${data.username ?? ""}`}</div>
-          <div className="w-full border-b border-slate-400 "/>
-          <ProfileFeed userId={data.id}/>
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">{`@${
+          data.username ?? data.externalUsername ?? "unknown"
+        }`}</div>
+        <div className="w-full border-b border-slate-400" />
+        <ProfileFeed userId={data.id} />
       </PageLayout>
     </>
   );
 };
 
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = generateSSGHelper();
 
+  const slug = context.params?.slug;
 
- export const getStaticProps: GetStaticProps = async (context) => {
-  const ssg = generateSSGHelper()
-  
-const slug = context.params?.slug;
+  if (typeof slug !== "string") throw new Error("no slug");
 
-if (typeof slug !== "string") throw new Error("no slug");
+  const username = slug.replace("@", "");
 
-const username = slug.replace("@", "");
+  await ssg.profile.getUserByUsername.prefetch({ username });
 
-await ssg.profile.getUserByUsername.prefetch({username});
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      username,
+    },
+  };
+};
 
-return {
-  props: {
-    trpcState: ssg.dehydrate(),
-    username,
-   }
-  }
- }
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
+};
 
- export const getStaticPaths = () => {
-   return {paths: [], fallback: "blocking"}
-
- }
-
-export default  ProfilePage;
+export default ProfilePage;
